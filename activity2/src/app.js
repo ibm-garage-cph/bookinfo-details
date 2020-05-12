e085b0865b6817fd890210f5205dc2f908c27c8a
Rafal Szypulka <rafal.szypulka@gmail.com>
2020-05-12 16:51:09 +0200

    Added logging and metrics instrumentation

--- activity2/src/app.js
+++ activity2/src/app.js
@@ -1,6 +1,36 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send({ message: "Hello World!" }));
+//Add Prometheus client library 
+const promBundle = require("express-prom-bundle");
+//Add logging library
+const { createLogger, format, transports } = require('winston')
+
+const metricsMiddleware = promBundle({
+    includeMethod: true,
+    includePath: true,
+    promClient: {
+        collectDefaultMetrics: {
+        }
+    }
+});
+
+const logger = createLogger({
+    level: 'debug',
+    format: format.combine(
+      format.timestamp({
+        format: "YYYY-MM-DD'T'HH:mm:ss.SSSZ"
+      }),
+      format.json()
+    ),
+    transports: [new transports.Console()]
+  });
+
+app.use(metricsMiddleware);
+
+app.get("/", (req, res) => {
+    res.send({ message: "Hello World!" })
+    logger.info("Hello World!", {"errCode": "DET00001I"})
+})
 
 module.exports = { app };
