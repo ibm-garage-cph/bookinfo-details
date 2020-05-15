ad874280ca3f7a5de2cfc5ced859ac17ff285b25
Mihai Nueleanu <mihai.nueleanu@ibm.com>
2020-05-15 17:41:39 +0200

    tekton pipeline finished

---
+++ activity4/0_secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry dockerhub \
+                    --docker-server=$DOCKER_SERVER \
+                    --docker-username=$DOCKER_USERNAME \
+                    --docker-password=$DOCKER_PASSWORD \
+                    --docker-email=$DOCKER_EMAIL
